77c99be3a663b8c9d8af094134606ace8c492abe
Namya LG <53875297+Namyalg@users.noreply.github.com>
2022-07-26 19:02:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# A simple endpoint that returns the list of attendees 
 @app.route('/')
 def index():
-    attendees = ['A', 'B', 'C']
+    attendees = ['A', 'B', 'C', 'D']
     return {'Attendees': attendees}
 
 
 if __name__ == "__main__":
+    # Runs on port 5000 by default
     app.run(debug=True, host='0.0.0.0')
